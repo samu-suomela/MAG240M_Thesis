ecadb1a2b9cd4fbe8e3f8ee1d61b55d56c4b9b67
samu-suomela11 <samusuomela@gmail.com>
2021-09-06 14:34:26 +0300

    renaming files and added igraph code

--- Code/label_propagation_test_data_igraph.py
+++ Code/label_propagation_test_data_igraph.py
@@ -2,16 +2,29 @@ from igraph import *
 import numpy as np
 import pandas as pd
 import time
-from ogb.lsc import MAG240MDataset
 
-dataset = MAG240MDataset(root = "/wrk/users/sjsuomel/data/")
-paper_to_paper = dataset.edge_index('paper', 'paper')
-labels = dataset.paper_label
+paper_to_paper = np.load("test_data_label_propagation/edgelist_cites_test_label_propagation.npy")
+papers = np.load("test_data_label_propagation/paper_array_test_label_propagation.npy")
+labels = np.load("test_data_label_propagation/paper_label_test_label_propagation.npy")
+
+edgelist_reindexed = []
+papers_reindexed = []
+mapping = {}
+for i in range(len(papers)):
+    mapping[papers[i]] = i
+    papers_reindexed.append(i)
+
+for i in range(len(paper_to_paper.T)):
+    edge = paper_to_paper[:,i]
+    edgelist_reindexed.append([mapping[edge[0]],mapping[edge[1]]])
+
+paper_to_paper = edgelist_reindexed
 
 print("Edges and labels loaded")
 
-G = igraph.Graph()
-G.add_edges(paper_to_paper.T)
+G = Graph(edges=paper_to_paper)
+
+print(G.vcount(), G.ecount())
 
 print("Graph created")
 
@@ -46,8 +59,9 @@ while True:
         print("Loopcount reached.")
         break
 
+print(np.unique(labels))
 print("Label propagation took %s seconds" % (time.time() - start_time))
 
-np.save("/wrk/users/sjsuomel/results/label_propagation_results", labels) # save predicted labels for evaluation
+#np.save("/wrk/users/sjsuomel/results/label_propagation_results", labels) # save predicted labels for evaluation
 
-print("File successfully saved!")
+#print("File successfully saved!")
